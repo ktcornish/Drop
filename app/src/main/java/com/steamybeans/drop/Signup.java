da603b3328f2f84f92378f236aa202a7827962d8
Katie Cornish <katielcornish@gmail.com>
2019-03-18 14:26:33 +0000

    Removed Nav and Action bar from sign up page

--- app/src/main/java/com/steamybeans/drop/Signup.java
+++ app/src/main/java/com/steamybeans/drop/Signup.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,6 +36,9 @@ public class Signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_signup);
         init();
