ce75cc8f375b3f84cadd7e014f73ed735b4d1834
Katie Cornish <katielcornish@gmail.com>
2019-03-18 11:44:26 +0000

    added signup page and button to pass test

---
+++ app/src/main/java/com/steamybeans/drop/Signup.java
@@ -0,0 +1,20 @@
+package com.steamybeans.drop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Signup extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+
+
+}}
