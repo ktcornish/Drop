6686ce246b4c4a57e8803726a04354d1ef41b300
Katie Cornish <katielcornish@gmail.com>
2019-03-18 12:05:32 +0000

    added test for typing in email and password on sign up

--- app/src/androidTest/java/com/steamybeans/drop/SIgnUpFeatureTest.java
+++ app/src/androidTest/java/com/steamybeans/drop/SIgnUpFeatureTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -23,6 +24,15 @@ public class SIgnUpFeatureTest {
     @Test
     public void clickSignUpButton_toOpenSignUpPage() throws Exception {
         onView(withId(R.id.BTNsignUp)).perform(click());
-        onView(withId(R.id.TVsignUp)).check(matches(isDisplayed()));
+        onView(withId(R.id.BTNcompleteSignUp)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void fillInEmailAndPassword() throws Exception {
+        clickSignUpButton_toOpenSignUpPage();
+        onView(withId(R.id.ETsignupEmailAddress)).perform(typeText("test@user.com"));
+        onView(withId(R.id.ETsignupPassword)).perform(typeText("password"));
+        onView(withId(R.id.BTNcompleteSignUp)).perform(click());
+        onView(withId(R.id.TVlogin)).check(matches(isDisplayed()));
     }
 }
--- app/src/main/java/com/steamybeans/drop/Signup.java
+++ app/src/main/java/com/steamybeans/drop/Signup.java
@@ -1,7 +1,10 @@
 package com.steamybeans.drop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -9,12 +12,25 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class Signup extends AppCompatActivity {
 
+    private Button BTNcompleteSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_signup);
+        init();
+    }
 
+    public void init() {
+        BTNcompleteSignUp = (Button)findViewById(R.id.BTNcompleteSignUp);
 
-}}
+        BTNcompleteSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this, LoginPage.class));
+            }
+        });
+    }
+}
