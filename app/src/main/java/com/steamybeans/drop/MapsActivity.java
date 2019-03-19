8b0711d2019898e826ff77004f57bcef2eb8a51c
Ross <ross.hunter@tesco.com>
2019-03-19 12:10:56 +0000

    Toast dummy location

--- app/src/main/java/com/steamybeans/drop/MapsActivity.java
+++ app/src/main/java/com/steamybeans/drop/MapsActivity.java
@@ -42,5 +42,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Toast location
+
+
     }
 }
--- app/src/main/java/com/steamybeans/drop/views/home_activity/HomeActivity.java
+++ app/src/main/java/com/steamybeans/drop/views/home_activity/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.steamybeans.drop.views.home_activity;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,7 +21,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.steamybeans.drop.R;
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
-
     private GoogleMap mMap;
 
     @Override
@@ -50,8 +54,19 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Loc to be reassignable - final only in test context
+        final LatLng makers = new LatLng(51.524579,-0.098996);
+        mMap.addMarker(new MarkerOptions().position(makers).title("Marker in Makers"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(makers));
+
+        //Toastloc
+        FloatingActionButton button = findViewById(R.id.fabTempToastLoc);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "" + makers.toString() , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
