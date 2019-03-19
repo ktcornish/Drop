93a3903311e19f91d42b49b7b433c17bc5b3cae6
sebastianlavender <sebastianlavender@outlook.com>
2019-03-19 13:33:19 +0000

    fixed issues

--- app/src/main/java/com/steamybeans/drop/views/HomeActivity.java
+++ app/src/main/java/com/steamybeans/drop/views/HomeActivity.java
@@ -1,15 +1,14 @@
 package com.steamybeans.drop.views;
 
 
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.app.Dialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
