4e5fef7dbd19397517ff093fc663cfa5efd3aa93
sebastianlavender <sebastianlavender@outlook.com>
2019-03-20 11:21:20 +0000

    Refactored LoginPage

--- app/src/main/java/com/steamybeans/drop/views/LoginPage.java
+++ app/src/main/java/com/steamybeans/drop/views/LoginPage.java
@@ -13,44 +13,34 @@ import android.widget.EditText;
 import com.steamybeans.drop.R;
 import com.steamybeans.drop.firebase.Authentication;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class LoginPage extends AppCompatActivity {
 
-    private Button BTNsignUp;
-    private FloatingActionButton BTNlogin;
-    private String email;
-    private String password;
-    private EditText ETemail;
-    private EditText ETpassword;
     private Authentication authentication;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_loginpage);
+
+        //check if user is already signed in
         authentication = new Authentication(this);
-        init();
+        authentication.alreadySignedIn();
+
+        setUpButtons();
         }
 
-        private void init() {
-        BTNsignUp = (Button)findViewById(R.id.BTNsignUp);
-        ETemail = (EditText)findViewById(R.id.ETloginEmailAddress);
-        ETpassword = (EditText)findViewById(R.id.ETloginPassword);
-        BTNlogin = (FloatingActionButton)findViewById(R.id.BTNlogin);
+        private void setUpButtons() {
+        //declare page items
+        Button BTNsignUp = findViewById(R.id.BTNsignUp);
+        FloatingActionButton BTNlogin = findViewById(R.id.BTNlogin);
+        final EditText ETemail = findViewById(R.id.ETloginEmailAddress);
+        final EditText ETpassword = findViewById(R.id.ETloginPassword);
 
         BTNsignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(LoginPage.this, Signup.class));
             }
         });
@@ -58,19 +48,11 @@ public class LoginPage extends AppCompatActivity {
         BTNlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                email = ETemail.getText().toString();
-                password = ETpassword.getText().toString();
+                String email = ETemail.getText().toString();
+                String password = ETpassword.getText().toString();
                 authentication.login(email, password);
             }
         });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        authentication = new Authentication(this);
-        authentication.alreadySignedIn();
     }
 
     @Override
