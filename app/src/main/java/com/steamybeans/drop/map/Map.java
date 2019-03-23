1cf8e4d2cbf75aaeab33732a9a75f2738856eee5
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-03-23 19:06:17 +0000

    can view new dialog when clicking on view comments btn, no need for a new view. showing just the new drop dialog box at the moment.

--- app/src/main/java/com/steamybeans/drop/map/Map.java
+++ app/src/main/java/com/steamybeans/drop/map/Map.java
@@ -65,6 +65,7 @@ public class Map extends AppCompatActivity {
 
                     Button BTNupvote = dialog.findViewById(R.id.BTNupvote);
                     Button BTNdownvote = dialog.findViewById(R.id.BTNdownvote);
+                    Button BTNviewComment = dialog.findViewById(R.id.BTNviewComments);
 
                     BTNupvote.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -76,6 +77,14 @@ public class Map extends AppCompatActivity {
                         @Override
                         public void onClick(View v) {
                             vote.makeAVote(-1, marker.getTitle(), marker.getSnippet(), TVvotes);
+                        }
+                    });
+                    BTNviewComment.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            final Dialog dialog1 = new Dialog(context);
+                            dialog.setContentView(R.layout.dialogue_new_drop);
+                            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                         }
                     });
 
