85003e5b02b4d557873d2b383aba65b3a0598f94
Ross <ross.hunter@tesco.com>
2019-03-25 16:55:15 +0000

    added FMC classes

---
+++ app/src/main/java/com/steamybeans/drop/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.steamybeans.drop.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyDropListenService";
+
+
+}
