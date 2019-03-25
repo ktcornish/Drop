4418beea95a5e00f2dc372aad9008b727ecb0d67
Sebastian Lavender <sebastianlavender@outlook.com>
2019-03-25 15:36:14 +0000

    notification for over 5 likes on a post

--- app/src/main/java/com/steamybeans/drop/firebase/Notification.java
+++ app/src/main/java/com/steamybeans/drop/firebase/Notification.java
@@ -1,4 +1,45 @@
 package com.steamybeans.drop.firebase;
 
-public class Notification {
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import com.steamybeans.drop.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class Notification extends AppCompatActivity {
+
+    private final Context context;
+
+    public Notification(Context context) {
+        this.context = context;
+    }
+
+    private static final String CHANNEL_ID = "drop";
+    private static final String CHANNEL_NAME = "Drop";
+    private static final String CHANNEL_DESC = "drop notifications";
+
+    public void createNotification(String title, String content) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_nav_drop_24dp)
+                        .setContentTitle(title)
+                        .setContentText(content)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(1, mBuilder.build());
+    }
+
+
+    public void displayNotification() {
+
+
+    }
+
+
 }
--- app/src/main/java/com/steamybeans/drop/views/HomeActivity.java
+++ app/src/main/java/com/steamybeans/drop/views/HomeActivity.java
@@ -3,6 +3,8 @@ package com.steamybeans.drop.views;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
@@ -55,6 +57,7 @@ import com.steamybeans.drop.R;
 import com.steamybeans.drop.firebase.Authentication;
 import com.steamybeans.drop.firebase.Drop;
 import com.steamybeans.drop.firebase.Firebasemarker;
+import com.steamybeans.drop.firebase.Notification;
 import com.steamybeans.drop.firebase.User;
 import com.steamybeans.drop.firebase.Vote;
 import com.steamybeans.drop.map.Map;
@@ -96,6 +99,13 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkUserLocationPermission();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("drop", "Drop", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("drop notifications");
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
         }
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -186,6 +196,51 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onResume();
         //check if user account is still active
         authentication.checkAccountIsActive();
+        final Notification notification = new Notification(this);
+
+        //check for changes to db
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("users")
+                .child(user.getUid()).child("posts");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    String dropUid = snapshot.getKey();
+
+                    FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid())
+                            .child("posts").child(dropUid).child("votes")
+                            .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            int counter = 0;
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                counter += snapshot.getValue(Integer.class);
+                            }
+                            if (counter == 5) {
+                                notification.createNotification("DROP", "Has received 5 likes");
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     // Populate toolbar with buttons
